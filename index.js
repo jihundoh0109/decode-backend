c72d652b27b525b5c2298121f64f58b14667e00f
Jihun (James) Doh <97568255+jihundoh0109@users.noreply.github.com>
2023-09-20 13:48:02 +0000

    done?

--- index.js
+++ index.js
@@ -26,7 +26,6 @@ function generatePrompt(code) {
 
 app.post("/api/decode", async (req, res) => {
   const selectedCode = req.body.selectedCode;
-  console.log(selectedCode)
   try {
     const completion = await openai.chat.completions.create({
       messages: [
